3da2b8b2dae41faada687013d6cf2d2e4c92aedf
Julian Krochmal <juliankrochmal@gmail.com>
2025-01-01 16:02:02 -0300

    fix: streamline Cypress configuration and ensure proper formatting in support files

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on, config) {
-    },
+    setupNodeEvents(on, config) {},
   },
 });
